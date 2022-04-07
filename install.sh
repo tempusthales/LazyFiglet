1e653df9d3e9e9c64ffc6ba7fee67932771c337c
Gilbert Palau <gilbertpalau@gmail.com>
2022-04-07 12:06:09 -0500

    Updated to support brew

--- install.sh
+++ install.sh
@@ -5,28 +5,28 @@ cd
 echo "\033[1;93m#Bismillah"
 figlet  "Bismillah"
 echo ""
-apt update
-apt upgrade
+brew update
+brew upgrade
 echo "$(tput setaf 5) Subscribe < Technical Mujeeb YT channel For more Termux videos "
 echo ""
 echo "$(tput setaf 10) Installing FIGLET......."
-apt install figlet
+brew install figlet
 echo "$(tput setaf 6) Installing TOILET......."
-apt install toilet
+brew install toilet
 echo ""
 echo "$(tput setaf 10) Installing COWSAY......."
 figlet -f Bubble "installing cowsay"
-apt install cowsay
+brew install cowsay
 echo ""
 echo "$(tput setaf 14) Installing NODE-JS......."
 figlet -f Bubble "installing NodeJs"
-apt install nodejs
+brew install nodejs
 echo ""
 echo "$(tput setaf 5) Installing FIGLET-cli......."
 npm install figlet
 
 figlet -f Bubble "installing figlet CLI"
-npm install -g figlet-cli
+npm install -g figlet-cli --force
 echo ""
 echo "\033[1;91m"
 
@@ -40,5 +40,6 @@ echo "$(tput setaf 3) Subscribe Technical Mujeeb.."
 echo "|-----------------------------|"
 echo ""
 echo "Now Run This Tool."
-cd LazyFiglet 
+
+exit 0
 
